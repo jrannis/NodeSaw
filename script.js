103d6f3547094954f3dd3fd31c6095e4b698fa5b
jrannis <jrannis89@gmail.com>
2026-07-18 23:18:13 -0400

    darkmode changes v2

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 const dark_button = document.getElementById("DarkMode_Button");
-const light_button = document.getElementById("LightMode_Button");
 
 
 dark_button.addEventListener("click",function(){
-    alert("Button Clicked!")
-});
-light_button.addEventListener("click",function(){
-    alert("Button Clicked!")
+    document.body.classList("dark-mode");
 });
