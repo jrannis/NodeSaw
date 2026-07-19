4db47845b865b99e42136c9d429cd4299f8cb68d
jrannis <jrannis89@gmail.com>
2026-07-18 23:05:49 -0400

    Adding dark/light mode functionality

---
+++ script.js
@@ -0,0 +1,10 @@
+const dark_button = document.getElementById("DarkMode_Button");
+const light_button = document.getElementById("LightMode_Button");
+
+
+dark_button.addEventListener("click",function(){
+    alert("Button Clicked!")
+});
+light_button.addEventListener("click",function(){
+    alert("Button Clicked!")
+});
